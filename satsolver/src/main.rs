5714b76494c383ef8f08a28ffeeea727a8f3a211
Nathaniel Victor <nv323@ic.ac.uk>
2024-07-13 15:40:20 +0100

    Written filename argument getting section

--- satsolver/src/main.rs
+++ satsolver/src/main.rs
@@ -1,3 +1,25 @@
+
+use std::{env::{self, Args}, process};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = get_filename(env::args()).unwrap_or_else(|message| {
+        eprintln!("{message}");
+        process::exit(1);
+    });
+
+    println!("{filename}");
+}
+
+// Get the filename, if provided
+fn get_filename(mut args: Args) -> Result<String, &'static str> {
+
+    // Skip Executable Name
+    args.next();
+
+    // Return filename if provided
+    // Or an error message if not
+    match args.next() {
+        Some(filename) => Ok(filename),
+        None => Err("Please provide a filename!"),
+    }
 }
